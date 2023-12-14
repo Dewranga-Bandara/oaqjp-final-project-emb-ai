605b49dd09f7adb5a9e421b10cd1c23f123ffc82
Dewranga-Bandara <dewrangabandara071@gmail.com>
2023-12-14 17:38:05 -0500

    Update server.py

--- server.py
+++ server.py
@@ -3,16 +3,22 @@ from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("EmotionDetector")
 
-@app.route("/")
 def render_index_page():
+    """
+    Renders the index page of the application.
+    """
     return render_template('index.html')
 
-@app.route("/emotionDetector", methods=['GET'])
 def emotion_detector_route():
+    """
+    Handles the emotionDetector route, processes the input, and returns the result.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyze)
+
     if result['dominant_emotion'] is None:
         return "Invalid text! Please try again."
+
     response_text = (
         f"For the given statement, the system response is "
         f"'anger': {result['anger']}, 'disgust': {result['disgust']}, "
@@ -22,6 +28,20 @@ def emotion_detector_route():
     response = make_response(response_text)
     response.headers['Content-Type'] = 'text/plain; charset=utf-8'
     return response
+
+@app.route("/")
+def index():
+    """
+    Entry point for the index route.
+    """
+    return render_index_page()
+
+@app.route("/emotionDetector", methods=['GET'])
+def emotion_detector_entry():
+    """
+    Entry point for the emotionDetector route.
+    """
+    return emotion_detector_route()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
